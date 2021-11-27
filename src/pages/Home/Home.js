6e737fc28d5a191ed42985a633ec36247ac0e10b
Subin <subinsk284@gmail.com>
2021-11-28 02:31:06 +0530

    bimg and ss added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,13 +6,26 @@ import Event from '../../components/Events/Event.js';
 import Footer from "../../components/Footer/Footer.js";
 
 import './Home.css';
+import One from '../../images/ss/1.png'
+import Two from '../../images/ss/2.png'
+import Three from '../../images/ss/3.jpg'
+import Four from '../../images/ss/4.png'
 
 import {
   container,
   bimg,
   introBox,
-  heading,
-  footerContainer
+  footerContainer,
+  introCard,
+  contentHeadingOne,
+  contentHeadingTwo,
+  contentAbout,
+  snapshots,
+  imagefirst,
+  imagesecond,
+  imagethird,
+  image
+
 } from './Home.module.css';
 
 const HomePage = () => {
@@ -22,11 +35,19 @@ const HomePage = () => {
 
         <div className={bimg}></div>
         <div className={introBox}>
-          <p className={heading}>Matrix.io</p>
-          <DiscordBtn url='https://discord.gg/Mjpjmdrk' />
-          <div className="introcard">
-            <div className="contentHeading">Matrix.io</div>
-            <div className="contentAbout"></div>
+          {/* <p className={heading}>Matrix.io</p> */}
+          {/* <DiscordBtn url='https://discord.gg/Mjpjmdrk' /> */}
+          <div className={introCard}>
+            <div className={contentHeadingOne}>Matrix.io</div>
+            <div className={contentHeadingTwo}>The Ultimate Community</div>
+            <hr style={{ width: '60%', color: '#fda611', borderStyle: 'solid', height: 0, boxSizing: 'content-box' }} />
+            <div className={contentAbout}>We empower not only the students as well as the communities which are part of our community.</div>
+          </div>
+          <div className={snapshots}>
+            <img className={image} id={imagefirst} src={One} alt="1" />
+            <img className={image} id={imagefirst} src={Two} alt="2" />
+            <img className={image} id={imagefirst} src={Three} alt="3" />
+            <img className={image} id={imagefirst} src={Four} alt="4" />
           </div>
         </div>
 
