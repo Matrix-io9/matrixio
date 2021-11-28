685c42ad13d04cb4d2c42522a8a71461b9fd4a29
Subin <subinsk284@gmail.com>
2021-11-28 17:36:35 +0530

    team desktop done

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -22,6 +22,7 @@ import MitaliLohar from '../../images/team/MitaliLohar.jpg'
 import CharchitAgrawal from '../../images/team/CharchitAgrawal.jpg'
 import ArchieParliya from '../../images/team/ArchieParliya.jpg'
 import KritiSharma from '../../images/team/KritiSharma.jpeg'
+import KaustubDuttPandey from '../../images/team/KaustubDuttPandey.jpg'
 
 const Team = () => {
     return (
@@ -43,6 +44,7 @@ const Team = () => {
                     <TeamCard name="Subin S K" desc="Head | MERN Stack Developer" src={SubinSK} alt='Subin S K' linkedin="https://www.linkedin.com/in/subin-s-k-9b767219a/" twitter="https://twitter.com/SubinSK6" />
                     <TeamCard name="Kriti Sharma" desc="Faculty Advisor" src={KritiSharma} alt='Kriti Sharma' linkedin="https://www.linkedin.com/in/subin-s-k-9b767219a/" twitter="" />
                     <TeamCard name="Chaitanya Agrawal" desc="Backend Developer" src={ChaitanyaAgrawal} alt='Chaitanya Agrawal' linkedin="https://www.linkedin.com/in/chaitanya-agarwal-4bb121186" twitter="https://twitter.com/Chaitanya_2002_" />
+                    <TeamCard name="Kaustub Pandey" desc="Backend Developer" src={KaustubDuttPandey} alt='Kaustub Pandey' linkedin="https://www.linkedin.com/in/kaustub-dutt-pandey-9921b920a/" twitter="https://twitter.com/PandeyKaustub" />
                     <TeamCard name="Mitali Lohar" desc="Python Developer" src={MitaliLohar} alt='Mitali Lohar' linkedin="https://www.linkedin.com/in/mitali-lohar-333204227" twitter=" https://twitter.com/mitali_lohar" />
                     <TeamCard name="Archie Parliya" desc="C++ Developer" src={ArchieParliya} alt='Archie Parliya' linkedin="https://www.linkedin.com/in/archie-parliya-295169217" twitter="https://twitter.com/ArchieParliya" />
                     <TeamCard name="Nishant Suthar" desc="Java Developer" src={NishantSuthar} alt='Nishant Suthar' linkedin="https://www.linkedin.com/in/nishant-suthar-302100215" twitter="https://twitter.com/NishantSuthar20" />
