3f6d6f21b482e99c32a927c7c76f58fcfa9e2b97
Subin <subinsk284@gmail.com>
2021-11-28 16:50:14 +0530

    team done

--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -1,21 +1,34 @@
 import React from "react";
 import {
-teamCard,
-image,
-cardTextBox,
-cardHeading,
-cardText
+    teamCard,
+    image,
+    cardTextBox,
+    cardHeading,
+    cardText,
+    socialMediaLinks,
+    icon,
+    link
 } from './TeamCard.module.css';
 
+import Twitter from '../../images/icons/twitter.png'
+import Linkedin from '../../images/icons/linkedin.png'
 const TeamCard = (props) => {
+    console.log(props.src)
     return (
-
         <div className={teamCard}>
             <img src={props.src} alt={props.alt} className={image} />
             <div className={cardTextBox}>
                 <p className={cardHeading}>{props.name}</p>
                 <hr />
                 <p className={cardText}>{props.desc}</p>
+            </div>
+            <div className={socialMediaLinks}>
+                <a href={props.linkedin} target="_blank" className={link} rel="noreferrer">
+                    <img className={icon} src={Linkedin} alt="linkedin"></img>
+                </a>
+                <a href={props.twitter} target="_blank" rel="noreferrer" className={link}>
+                    <img className={icon} src={Twitter} alt="twitter"></img>
+                </a>
             </div>
         </div>
     )
--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -2,34 +2,52 @@ import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import TeamCard from "../../components/TeamCard/TeamCard";
-import
-{
-  teamContainer,
-  teamHeading,
+import {
+    teamContainer,
     teamCards,
-    
+    teamContentIntro,
+    teamContentAbout,
+    teamContentHeading,
+
 } from './Team.module.css';
 
+import './Team.css';
 import Main from "../../layouts/Main";
+
+import GarvitChouhan from '../../images/team/GarvitChouhan.png'
+import SubinSK from '../../images/team/SubinSK.jpg'
+// import ChaitanyaAgrawal from '../../images/team/ChaitanyaAgrawal.' 
+import NishantSuthar from '../../images/team/NishantSuthar.jpeg'
+import MitaliLohar from '../../images/team/MitaliLohar.jpg'
+import CharchitAgrawal from '../../images/team/CharchitAgrawal.jpg'
+import ArchieParliya from '../../images/team/ArchieParliya.jpg'
 
 const Team = () => {
     return (
         <Main description={"Team behind Matrix.io Student Comumunity"}>
-        <div>
-            <Navbar toggleText='Home' toggleLink=''/>
-            <div>
-                <div className={teamContainer}>
-                    <p className={teamHeading}>
-                        Team
-                    </p>
-                    <div className={teamCards}>
-                        <TeamCard name="Garvit Chouhan" desc="Co-Founder Matrix.io" src='./src/images/team/GarvitChouhan.png' alt='Garvit Chouhan' />
-                        <TeamCard name="Subin S K" desc="Co-Founder Matrix.io" src='./src/images/team/SubinSK.png' alt='Subin S K' />
-                    </div>
+            <Navbar toggleText='Home' toggleLink='' />
+
+            <div className={teamContentIntro}>
+                <div className={teamContentHeading}>
+                    Team
+                </div>
+                <div className={teamContentAbout}>
+                    The team which makes Matrix.io a big success.
+                </div>
+            </div>
+
+            <div className={teamContainer}>
+                <div className={teamCards}>
+                    <TeamCard name="Garvit Chouhan" desc="Head | AI/ML Developer" src={GarvitChouhan} alt='Garvit Chouhan' linkedin="https://www.linkedin.com/in/garvit9000/" twitter="" />
+                    <TeamCard name="Subin S K" desc="Head | MERN Stack Developer" src={SubinSK} alt='Subin S K' linkedin="https://www.linkedin.com/in/subin-s-k-9b767219a/" twitter="https://twitter.com/SubinSK6" />
+                    {/* <TeamCard name="Chaitanya Agrawal" desc="Python Backend Developer" src={ChaitanyaAgrawal} alt='Chaitanya Agrawal' linkedin="https://www.linkedin.com/in/chaitanya-agarwal-4bb121186"/> */}
+                    <TeamCard name="Charchit Agrawal" desc="Java Developer" src={CharchitAgrawal} alt='Charchit Agrawal' linkedin="https://www.linkedin.com/in/charchit-agarwal-a8161520b" twitter="https://twitter.com/charchit957" />
+                    <TeamCard name="Nishant Suthar" desc="Java Developer" src={NishantSuthar} alt='Nishant Suthar' linkedin="https://www.linkedin.com/in/nishant-suthar-302100215" twitter="https://twitter.com/NishantSuthar20" />
+                    <TeamCard name="Mitali Lohar" desc="Python Developer" src={MitaliLohar} alt='Mitali Lohar' />
+                    <TeamCard name="Archie Parliya" desc="C++ Developer" src={ArchieParliya} alt='Archie Parliya' />
                 </div>
             </div>
             <Footer />
-        </div>
         </Main>
     )
 }
