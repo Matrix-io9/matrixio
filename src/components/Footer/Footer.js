317167838494a1e64deb22c6d06756d0fb42e0f5
Subin S K <subinsk284@gmail.com>
2022-03-20 23:07:34 +0530

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,17 @@
 import React from "react";
 import {
     footer,
-    link
 }
     from './Footer.module.css';
 import Icon from "../Icons/Icon.js";
 
 const Footer = () => {
+    const date = new Date().getFullYear();
     return (
         <div className={footer}>
             <p>
-                Made with ❤ by&nbsp;
-                <a className={link} href="https://www.linkedin.com/in/subin-s-k-9b767219a/" rel="noreferrer" target="_blank">
-                    Subin
-                </a>
+                Copyright &copy; {date}
+                <br />
             </p>
             <div>
                 <Icon />
@@ -21,5 +19,4 @@ const Footer = () => {
         </div>
     )
 }
-// i'm samsy subin
 export default Footer;
