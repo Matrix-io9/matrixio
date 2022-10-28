54a4ed51ab5be18aa244e642be9f71d31fdadac2
sujalsahu <sujalsahu0804@gmail.com>
2022-10-28 09:26:49 +0530

    remove icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,6 +9,13 @@ const Footer = () => {
     const date = new Date().getFullYear();
     return (
         <div className={footer}>
+            {/* <div className="nav-links">
+                <a href className="link-1">Home</a>
+                <a href="/team" className="link-1">Team</a>
+                <a href="#events" className="link-1">Events</a>
+                <a href="#about" className="link-1">About</a>
+                <a href="#partners" className="link-1">Partners</a>
+            </div> */}
             <p>
                 Copyright &copy; {date}
                 <br />
